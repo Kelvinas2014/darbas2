805304826fe946e7009fbf6eefb282c53bd0b1df
Kelvinas2014 <Kelvinas2014@users.noreply.github.com>
2020-12-07 22:18:09 +0200

    Add project files.

---
+++ darbas2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.
+    Forms;
+
+namespace darbas2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        int sum = 0;
+        int i = 0;
+        int[] numbers = new int[10];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (i < 10)
+            {
+                numbers[i] = Convert.ToInt32(textBox1.Text);
+                i++;
+                MessageBox.Show("įvesti" +
+                    " papildomus skaičius");
+            }
+            else
+            {
+                for (int i = 0; i < numbers
+                    .Length; i++)
+                {
+                    sum += numbers[i];
+                }
+                MessageBox.Show(sum.ToString());
+            }
+                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
